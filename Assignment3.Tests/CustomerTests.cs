81a50b546d79897cb0085bef6e35830028aa9cf9
Joseph Gutierrez <jmgutierrez3@buffs.wtamu.edu>
2022-02-13 20:35:11 -0600

    UnitTest

---
+++ Assignment3.Tests/CustomerTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Assignment3.Models;
+
+namespace Assignment3.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+
+    public void Test1()
+    {
+
+        var Cust = new Customer()
+        {
+            Name = "john doe",
+            Address = "2099 South lane",
+            PhoneNum = "806-445-2227",
+            Email = "john@yahoo.com" 
+        };
+
+        //Test would check the customer name to profanity
+
+        Assert.NotEqual("Profanity", Cust.Name);
+
+    }
+}
